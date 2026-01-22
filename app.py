55a3b292075c1c5f228c7b912d0ff3959f27cac5
Andrey Pylov <pylovvandrej@gmail.com>
2026-01-22 21:47:20 +0700

    The working day(

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world(a,b):
-  return render_template('index.html', title=a, data=b)
+def hello_world():
+  return render_template('index.html')
+@app.errorhandler(404)
+def error(e):
+  return render_template('error.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
