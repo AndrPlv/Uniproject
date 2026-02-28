451778f01715301308f46fa6151ad7627cc3b7e8
Andrey Pylov <pylovvandrej@gmail.com>
2026-02-28 23:41:45 +0700

    Create config_client.py. The file is settings for station

---
+++ config_client.py
@@ -0,0 +1,16 @@
+import customtkinter
+
+root = customtkinter.CTk()
+
+root.title("Переконфигурация станции")
+
+customtkinter.set_appearance_mode('dark') # цвет фона
+customtkinter.set_default_color_theme('green') # цвет интерфейса
+root.geometry('300x400')
+root.resizable(0,0)
+
+
+
+
+root.mainloop()
+
