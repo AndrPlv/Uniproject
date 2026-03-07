3b1c19d7ee9d147dd9cf70254ad18a0e28bf97d1
Andrey Pylov <pylovvandrej@gmail.com>
2026-03-07 21:34:38 +0700

    MVP created

--- analitic.py
+++ analitic.py
@@ -1,3 +1,20 @@
-import time
+import plotly.graph_objects as go
+import pandas as pd
 
-print(time.timezone(8))
+fig = go.Figure()
+
+base = pd.read_csv('DataSet.csv', delimiter=";")
+fig.add_trace(go.Scatter(
+    x=[j for j in range(len(base))],
+    y=base["Temperature"],
+    mode='lines+markers+text',           # только линии
+    name='Название линии'
+))
+fig.add_trace(go.Scatter(
+    x=[j for j in range(len(base))],
+    y=base["Humidity"],
+    mode='lines+markers+text',           # только линии
+    name='Название линии'
+))
+
+fig.show()  # Показать
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from datetime import datetime
+import plotly.graph_objects as go
+import plotly.express as px
 import pandas as pd
 
 app = Flask(__name__)
@@ -8,18 +10,56 @@ app = Flask(__name__)
 @app.route('/input', methods=['POST'])
 def handle_data():
   data = request.get_json()
-  print(data)
-  dase = pd.read_csv("DataSet.csv", delimiter=";")
-  dase.loc[len(dase)] = data
-  dase.to_csv("DataSet.csv", index=False, sep=";")
+
+  if None in [data[j] for j in data.keys()]:
+    print("None!")
+    return {'code': 0}
+  else:
+    data["Time"] = pd.to_datetime(data["Time"], format="%d.%m.%Y %H:%M:%S")
+    dase = pd.read_csv("DataSet.csv", delimiter=";")
+    dase.loc[len(dase)] = data
+    dase.to_csv("DataSet.csv", index=False, sep=";")
   
   return {'code': 200}
+@app.route('/update')
+def update():
+  base = pd.read_csv('DataSet.csv', delimiter=";")
+  
+  return jsonify({
+        'Table': base[-15:][::-1].to_html(index=False),
+        'Temperature': [int(t) for t in base['Temperature'][-len(base):]],  
+        'Humidity': [int(h) for h in base['Humidity'][-len(base):]],       
+        'Time': pd.to_datetime(base['Time']).to_list()
+    })
+  
 @app.route('/')
 def main():
   base = pd.read_csv('DataSet.csv', delimiter=";")
-  data = (base[len(base)-25:][::-1]).to_html()
-  return render_template('index.html', title="EcoView", info=data, now_time=datetime.now().strftime("Сегодня %d.%m.%Y, время %H:%M"))  
 
+  table = base[-15:].to_html()
+
+  Temgrapch = go.Figure()
+  Temgrapch.add_trace(go.Scatter(
+    x=base["Time"],
+    y=base["Temperature"],
+    mode='lines',
+    name="Temperature"
+  ))
+
+  Humgrapch = go.Figure()
+  Humgrapch.add_trace(go.Scatter(
+    x=base["Time"],
+    y=base["Humidity"],
+    mode='lines',
+    name="Humidity"
+  ))
+
+  Temgrapch = Temgrapch.to_html(full_html=False, include_plotlyjs=False)
+  Humgrapch = Humgrapch.to_html(full_html=False, include_plotlyjs=False)
+
+  return render_template("index.html", table=table, Temgrapch=Temgrapch, Humgrapch=Humgrapch)
+    
+ 
 @app.errorhandler(404)
 def error(e):
   return render_template('error.html')
